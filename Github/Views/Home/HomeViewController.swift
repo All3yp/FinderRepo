b6a64c4ffdbc1fe4f0a345864351e08816fdba0c
All3yp <noyallepereira@gmail.com>
2022-03-31 17:22:15 -0300

    feat: Create tabbar controller on project

--- Github/Views/Home/HomeViewController.swift
+++ Github/Views/Home/HomeViewController.swift
@@ -22,11 +22,19 @@ class HomeViewController: UIViewController {
         }
     }
 
+	init(titleNav: String) {
+		super.init(nibName: nil, bundle: nil)
+		title = titleNav
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		view.backgroundColor = .orange
-        
+		view.backgroundColor = .systemBackground
         getInitialRepositories()
 	}
 
