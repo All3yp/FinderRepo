970b8261daa237b68dd34ce8e9b3888f8a996941
Alison Gustavo de Lima <alisonglima.dev@gmail.com>
2022-03-31 12:04:29 -0300

    feat: add model FavoriteRepository

--- Github/Model/FavoriteRepository.swift
+++ Github/Model/FavoriteRepository.swift
@@ -1,8 +1,15 @@
-//
-//  FavoriteRepository.swift
-//  Github
-//
-//  Created by FL00022 on 31/03/22.
-//
-
 import Foundation
+
+struct FavoriteRepository {
+    let id: Int
+    let name: String
+    let repositoryDescription: String
+    let linkAvatar: String
+    let repositoryUrl: String
+    let ownerName: String
+    let watchersCount: Int
+    let repositoryCreatedAt: Date
+    let licenseTypeName: String
+    let createdAt: Date
+    let isFavorite: Bool
+}
