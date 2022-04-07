c12c480bed67f2db510cb52e45ee6dd862c9e8cc
All3yp <noyallepereira@gmail.com>
2022-04-07 18:10:22 -0300

    lint warnings

--- Github/Application/SceneDelegate.swift
+++ Github/Application/SceneDelegate.swift
@@ -8,35 +8,35 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
-    
-    let rootController = CustomTabBarController()
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = rootController
-        window?.makeKeyAndVisible()
+	var window: UIWindow?
+	let rootController = CustomTabBarController()
 
-        if #available(iOS 13.0, *) {
-            if UITraitCollection.current.userInterfaceStyle == .dark {
-                window?.overrideUserInterfaceStyle = .light
-            }
-        }
-    }
+	func scene(
+		_ scene: UIScene, willConnectTo session: UISceneSession,
+		options connectionOptions: UIScene.ConnectionOptions
+	) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		window = UIWindow(windowScene: windowScene)
+		window?.rootViewController = rootController
+		window?.makeKeyAndVisible()
 
-    func sceneDidDisconnect(_ scene: UIScene) {}
+		if #available(iOS 13.0, *) {
+			if UITraitCollection.current.userInterfaceStyle == .dark {
+				window?.overrideUserInterfaceStyle = .light
+			}
+		}
+	}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {}
+	func sceneDidDisconnect(_ scene: UIScene) {}
 
-    func sceneWillResignActive(_ scene: UIScene) {}
+	func sceneDidBecomeActive(_ scene: UIScene) {}
 
-    func sceneWillEnterForeground(_ scene: UIScene) {}
+	func sceneWillResignActive(_ scene: UIScene) {}
 
-    }
-    
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-    }
+	func sceneWillEnterForeground(_ scene: UIScene) {}
+
+}
+
+func sceneDidEnterBackground(_ scene: UIScene) {
+	(UIApplication.shared.delegate as? AppDelegate)?.saveContext()
 }
