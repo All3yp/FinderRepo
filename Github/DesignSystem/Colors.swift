355ea4f05c7df5ed2e233ed3fe50ddfc1d0d0efd
renato <felicio95.jr@gmail.com>
2022-04-07 00:29:35 -0300

    Adds Initial colors for app

---
+++ Github/DesignSystem/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Github
+//
+//  Created by Renato F. dos Santos Jr on 06/04/22.
+//
+
+import Foundation
+import UIKit
+
+struct TabBarColors {
+    static let backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+}
+
+struct HomeColors {
+    static let backgroundColor = UIColor.white
+}
