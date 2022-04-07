574c8a6342261eabcb7c010272463416ba01653a
Alison Gustavo de Lima <alisonglima.dev@gmail.com>
2022-04-07 17:58:50 -0300

    fix: force white mode theme

--- Github/Application/SceneDelegate.swift
+++ Github/Application/SceneDelegate.swift
@@ -8,28 +8,34 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-	var window: UIWindow?
-
-	let rootController = CustomTabBarController()
-
-	func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
-			     options connectionOptions: UIScene.ConnectionOptions) {
-		guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    var window: UIWindow?
+    
+    let rootController = CustomTabBarController()
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-		window?.rootViewController = rootController
-		window?.makeKeyAndVisible()
-	}
-
-	func sceneDidDisconnect(_ scene: UIScene) {}
-
-	func sceneDidBecomeActive(_ scene: UIScene) {}
-
-	func sceneWillResignActive(_ scene: UIScene) {}
-
-	func sceneWillEnterForeground(_ scene: UIScene) {}
-
-	func sceneDidEnterBackground(_ scene: UIScene) {
-		(UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-	}
+        window?.rootViewController = rootController
+        window?.makeKeyAndVisible()
+    }
+    
+    if #available(iOS 13.0, *) {
+        if UITraitCollection.current.userInterfaceStyle == .dark {
+            window?.overrideUserInterfaceStyle = .light
+        }
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    
+    func sceneWillResignActive(_ scene: UIScene) {}
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
 }
