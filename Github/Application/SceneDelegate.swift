3c24ac82cdee0f90b3dfbaca0ed0a2b67cd31c93
Alison Gustavo de Lima <alisonglima.dev@gmail.com>
2022-04-07 18:03:54 -0300

    fix: force white mode theme and fix swiftlint warnings

--- Github/Application/SceneDelegate.swift
+++ Github/Application/SceneDelegate.swift
@@ -3,38 +3,37 @@
 //  Github
 //
 //  Created by Alley Pereira on 23/03/22.
-// swiftlint:disable vertical_parameter_alignment
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
+
     var window: UIWindow?
-    
+
     let rootController = CustomTabBarController()
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = rootController
         window?.makeKeyAndVisible()
-    }
-    
-    if #available(iOS 13.0, *) {
-        if UITraitCollection.current.userInterfaceStyle == .dark {
-            window?.overrideUserInterfaceStyle = .light
+
+        if #available(iOS 13.0, *) {
+            if UITraitCollection.current.userInterfaceStyle == .dark {
+                window?.overrideUserInterfaceStyle = .light
+            }
         }
     }
-    
+
     func sceneDidDisconnect(_ scene: UIScene) {}
-    
+
     func sceneDidBecomeActive(_ scene: UIScene) {}
-    
+
     func sceneWillResignActive(_ scene: UIScene) {}
-    
+
     func sceneWillEnterForeground(_ scene: UIScene) {}
-    
+
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
